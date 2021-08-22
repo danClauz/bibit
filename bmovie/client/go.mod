3c24211e09fbe49b998b78114e12e9d7b7472a33
kaylerangsi <danny.ferian@ovo.id>
2021-08-23 01:04:58 +0700

    search movie client test

---
+++ bmovie/client/go.mod
@@ -0,0 +1,9 @@
+module github.com/danClauz/bibit/bmovie/client
+
+go 1.16
+
+require (
+	github.com/danClauz/bibit/bmovie/search v0.0.0-20210822175614-d7e6e881097c
+	github.com/golang/glog v1.0.0
+	google.golang.org/grpc v1.40.0
+)
---
+++ bmovie/client/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"context"
+	searchpb "github.com/danClauz/bibit/bmovie/search/gen"
+	"github.com/golang/glog"
+	"google.golang.org/grpc"
+	"log"
+)
+
+func main() {
+	defer glog.Flush()
+	ctx := context.Background()
+
+	cc, err := grpc.Dial("127.0.0.1:2435", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalln("failed to connect", err)
+	}
+	defer cc.Close()
+
+	c := searchpb.NewSearchServiceClient(cc)
+
+	searchResp, err := c.SearchMovie(ctx, &searchpb.SearchMovieRequest{
+		Search: "batman",
+		Page:   1,
+	})
+
+	if err != nil {
+		glog.Fatalln(err)
+	}
+
+	glog.Info(searchResp)
+
+	detailResp, err := c.DetailMovie(ctx, &searchpb.DetailMovieRequest{
+		ImdbId: searchResp.GetSearch()[0].ImdbId,
+	})
+
+	if err != nil {
+		glog.Fatalln(err)
+	}
+
+	glog.Info(detailResp)
+}
